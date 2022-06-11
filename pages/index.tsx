717c9217c76115934dd3b4b29f8ac1172aab6ef7
Camilo López <camiloj.lopez79@gmail.com>
2022-06-10 21:07:42 -0400

    NextUI - React UI Library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next'
+import { Button } from '@nextui-org/react';
 
 const HomePage: NextPage = () => {
   return (
     <>
       <h1>Hello World</h1>
+      <Button
+        color='gradient'
+      >
+        Hello
+      </Button>
     </>
   )
 }
