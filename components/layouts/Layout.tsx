e1f6e1042ce50bb2789b54186d098a49fd74d634
Camilo López <camiloj.lopez79@gmail.com>
2022-06-21 14:15:53 -0400

    Creando una barra de navegación

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react';
 import Head from 'next/head';
+import {Navbar} from '../ui/Navbar';
 
 interface Props {
   children: ReactNode;
@@ -16,9 +17,11 @@ export const Layout: FC<Props> = ({ children, title }) => {
         <meta name='keywords' content={ `${ title }, pokemon, pokedex` } />
       </Head>
 
-      { /* Navbar */ }
+      <Navbar />
 
-      <main>
+      <main style={{
+        padding: '0px 20px'
+      }}>
         { children }
       </main>
     </>
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,4 +1,9 @@
+import { Spacer, Text, useTheme } from "@nextui-org/react";
+import Image from "next/image";
+
 export const Navbar = () => {
+  const { theme } = useTheme();
+
   return (
     <div style={{
       display: 'flex',
@@ -7,9 +12,21 @@ export const Navbar = () => {
       alignItems: 'center',
       justifyContent: 'start',
       padding: '0px 20px',
-      backgroundColor: 'red'
+      backgroundColor: theme?.colors.gray200.value
     }}>
-      <span>Hello</span>
+      <Image 
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+        alt="App Icon"
+        width={ 70 }
+        height={ 70 }
+      />
+
+      <Text color="white" h2>P</Text>
+      <Text color="white" h3>okemon</Text>
+
+      <Spacer css={{ flex: 1 }} />
+
+      <Text color="white">Favorites</Text>
     </div>
   );
 }
