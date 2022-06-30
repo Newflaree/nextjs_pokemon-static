c8cf85394fba73c17cb632656ae61688ee3fddc8
Camilo López <camiloj.lopez79@gmail.com>
2022-06-29 23:20:56 -0400

    Next - GetStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import { Button } from '@nextui-org/react';
 import { Layout } from '../components/layouts';
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = ( props ) => {
+  console.log({ props });
   return (
     <Layout
       title='Pokes'
@@ -15,6 +16,16 @@ const HomePage: NextPage = () => {
       </Button>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async ( ctx ) => {
+  console.log( 'Hello' );
+
+  return {
+    props: {
+      name: 'DD'
+    }
+  }
 }
 
 export default HomePage;
