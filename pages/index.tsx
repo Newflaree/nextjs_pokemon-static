3f5d63771d1b92d7f963381b46959763f988f22c
Camilo López <camiloj.lopez79@gmail.com>
2022-06-30 00:24:10 -0400

    Tarea - Mostrar nombre y número del pokemon

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,32 +3,45 @@ import { Button } from '@nextui-org/react';
 // Api
 import { pokeApi } from '../api';
 // Interfaces
-import { PokemonListResponse } from '../interfaces';
+import { PokemonListResponse, SmallPokemon } from '../interfaces';
 // Components
 import { Layout } from '../components/layouts';
 
-const HomePage: NextPage = ( props ) => {
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
+  console.log( pokemons[0].name );
 
   return (
     <Layout
       title='Pokes'
     >
-      <h1>Hello World</h1>
-      <Button
-        color='gradient'
-      >
-        Hello
-      </Button>
+      <ul>
+        {
+          pokemons.map( ({ id, name }) => (
+            <li key={ id }>
+              #{ id } - { name }
+            </li>
+          ))
+        }
+      </ul>
     </Layout>
   )
 }
 
 export const getStaticProps: GetStaticProps = async ( ctx ) => {
   const { data } = await pokeApi.get<PokemonListResponse>( '/pokemon?limit=151' )
+  const pokemons: SmallPokemon[] = data.results.map( (poke, i) => ({
+    ...poke,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ i + 1 }.png`
+  }));
 
   return {
     props: {
-      pokemons: data.results
+      pokemons
     }
   }
 }
