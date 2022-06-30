b85d7439ef2cd274b56a9be022a6bd2c88fa99ef
Camilo López <camiloj.lopez79@gmail.com>
2022-06-30 15:34:31 -0400

    Mostrar tarjetas de Pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { GetStaticProps, NextPage } from 'next'
-import { Button } from '@nextui-org/react';
+import { Card, Grid, Row, Text } from '@nextui-org/react';
 // Api
 import { pokeApi } from '../api';
-// Interfaces
-import { PokemonListResponse, SmallPokemon } from '../interfaces';
 // Components
 import { Layout } from '../components/layouts';
+// Interfaces
+import { PokemonListResponse, SmallPokemon } from '../interfaces';
 
 interface Props {
   pokemons: SmallPokemon[];
@@ -18,21 +18,44 @@ const HomePage: NextPage<Props> = ({ pokemons }) => {
     <Layout
       title='Pokes'
     >
-      <ul>
+      <Grid.Container gap={ 2 } justify='flex-start'>
         {
-          pokemons.map( ({ id, name }) => (
-            <li key={ id }>
-              #{ id } - { name }
-            </li>
+          pokemons.map( ({ id, name, img }) => (
+            <Grid
+              xs={ 6 }
+              sm={ 3 }
+              md={ 2 }
+              xl={ 1 }
+              key={ id }
+            >
+              <Card
+                isHoverable
+              >
+                <Card.Body css={{ p: 1 }}>
+                  <Card.Image 
+                    src={ img }
+                    width="100%"
+                    height={ 140 }
+                  />
+                </Card.Body>
+                <Card.Footer>
+                  <Row justify='space-between'>
+                    <Text transform='capitalize'>{ name }</Text>
+                    <Text>#{ id }</Text>
+                  </Row>
+                </Card.Footer>
+
+              </Card>
+            </Grid>
           ))
         }
-      </ul>
+      </Grid.Container>
     </Layout>
   )
 }
 
 export const getStaticProps: GetStaticProps = async ( ctx ) => {
-  const { data } = await pokeApi.get<PokemonListResponse>( '/pokemon?limit=151' )
+  const { data } = await pokeApi.get<PokemonListResponse>( '/pokemon?limit=151' );
   const pokemons: SmallPokemon[] = data.results.map( (poke, i) => ({
     ...poke,
     id: i + 1,
